8f6a58336cebfadf4f7e7ea399cadc918c5dce46
Danil Borchevkin <danil.borchevkin@gmail.com>
2024-11-09 02:36:29 +0300

    Minor fixes in 0242

--- 0242_valid-anagram_cpp/0242_valid-anagram.cpp
+++ 0242_valid-anagram_cpp/0242_valid-anagram.cpp
@@ -1,33 +1,34 @@
-#include <string>
+// Link to the problem - https://leetcode.com/problems/valid-anagram/description/
+
 #include <cassert>
+#include <string>
 #include <unordered_map>
 
 using std::string;
 using std::unordered_map;
 
+
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-        unordered_map<char, int> hash_map_s;
-        unordered_map<char, int> hash_map_t;
+  bool isAnagram(string s, string t) {
+    unordered_map<char, int> hash_map_s;
+    unordered_map<char, int> hash_map_t;
 
-        this->fillMap(s, hash_map_s);
-        this->fillMap(t, hash_map_t);
+    this->fillMap(s, hash_map_s);
+    this->fillMap(t, hash_map_t);
 
+    return hash_map_s == hash_map_t;
+  }
 
-
-        return hash_map_s == hash_map_t;
+  void fillMap(const string &str, unordered_map<char, int> &map) {
+    for (const char &ch : str) {
+      if (map.count(ch)) {
+        map[ch] += 1;
+      } else {
+        map[ch] = 0;
+      }
     }
-
-    void fillMap(const string& str, unordered_map<char, int>& map) {
-        for (const char& ch : str) {
-            if (map.count(ch)) {
-                map[ch] += 1;
-            } else {
-                map[ch] = 0;
-            }
-        }
-    }
+  }
 };
 
 void test() {
